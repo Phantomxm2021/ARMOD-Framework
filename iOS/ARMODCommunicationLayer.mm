c1691cf29ba89f49ada5c7b3c7f4775c5e983c35
幻象Phantoms <82647748+Phantomxm2021@users.noreply.github.com>
2021-05-26 16:41:26 +0800

    Update and rename NSObject+ARMOD.mm to ARMODCommunicationLayer.mm

--- iOS/ARMODCommunicationLayer.mm
+++ iOS/ARMODCommunicationLayer.mm
@@ -1,36 +1,47 @@
 //
-//  NSObject+ARMOD.m
-//  ARMODLib
+//  ARMODCommunicationLayer.m
+//  ARMOD
 //
-//  Created by NSWell on 2020/10/22.
+//  Created by phantomsxr on 2021/5/26.
+//  Copyright © 2020 phantomsxr.com. All rights reserved.
 //
 
-#import "NSObject+ARMOD.h"
-
+#import <Foundation/Foundation.h>
+#import "ARMODCommunicationLayer.h"
 @implementation ARMOD
-
+id<NativeCallsProtocol> ncp;
 NSDictionary* appLaunchOpts;
+
 static ARMOD *sharedInstance;
-
-
 static UIWindow *originWindow;
 
-char const *initApp="InitSDK";
-char const *fetchProject= "LaunchARQuery";
-char const *fetchProjectByImage= "LaunchARScanner";
-char const *appDelegate = "AppDelegate";
-char const *dataBundleId= "com.unity3d.framework";
+// MARK: - Methods Key
+char const *initApp                         = "InitSDK";
+char const *dispose                         = "Dispose";
+char const *cleanCache                      = "CleanCache";
+char const *fetchProject                    = "LaunchARQuery";
+char const *fetchProjectByImage             = "LaunchARScanner";
+char const *setUIInterfaceOrientation       = "SetUIInterfaceOrientation";
+char const *continueToDownloadARExperience  = "ContinueToDownloadAssets";
 
-id<NativeCallsProtocol> ncp;
-NSString *frameworkLibAPI = @"FrameworkLibAPI";
-NSString *bundlePath = @"/Frameworks/UnityFramework.framework";
+char const *appDelegate                     = "AppDelegate";
+
+// MARK: - Fields Key
+char const *messageReceiveObjecgtName       = "EntryPoint";
+char const *dataBundleId                    = "com.unity3d.framework";
+NSString *frameworkLibAPI                   = @"FrameworkLibAPI";
+NSString *bundlePathStr                     = @"/Frameworks/UnityFramework.framework";
 
 
+
+/*!
+@Discussion Load Unity Framework
+*/
 UnityFramework* UnityFrameworkLoad()
 {
     NSString* bundlePath = nil;
     bundlePath = [[NSBundle mainBundle] bundlePath];
-    bundlePath = [bundlePath stringByAppendingString: @"/Frameworks/UnityFramework.framework"];
+    bundlePath = [bundlePath stringByAppendingString: bundlePathStr];
     
     NSBundle* bundle = [NSBundle bundleWithPath: bundlePath];
     if ([bundle isLoaded] == false) [bundle load];
@@ -44,44 +55,43 @@ UnityFramework* UnityFrameworkLoad()
     return ufw;
 }
 
+/*!
+@Discussion Check current armod sdk is initialized
+*/
 - (bool)armodIsInitialized { return [self ufw] && [[self ufw] appController]; }
 
 
-/// Init ARMOD SDK Module
+/*!
+@Discussion Init ARMOD SDK Module
+*/
 - (void)initARMODModule{
     @try {
         if([self armodIsInitialized]) {
-            NSLog(@"ARMOD already initialized Unload ARMOD first");
             return;
         }
     
-
         [self setUfw: UnityFrameworkLoad()];
-        
-        [[self ufw] setDataBundleId: "com.unity3d.framework"];
-        
-
+    
+        [[self ufw] setDataBundleId: dataBundleId];
+    
         
         [[self ufw] registerFrameworkListener: self];
         
         if (ncp!=nil) {
-            [NSClassFromString(@"FrameworkLibAPI") registerAPIforNativeCalls:ncp];
+            [NSClassFromString(frameworkLibAPI) registerAPIforNativeCalls:ncp];
         }
         
         [[self ufw] runEmbeddedWithArgc: self.selfgArgc argv: self.selfgArgv appLaunchOpts: appLaunchOpts];
     
-        
         // set quit handler to change default behavior of exit app
         [[self ufw] appController].quitHandler = ^(){ NSLog(@"AppController.quitHandler called"); };
     
-        
         if (@available(iOS 13.0, *)) {
             if(originWindow.windowScene != nil){
                 [[[[self ufw] appController]  window] setWindowScene:originWindow.windowScene];
                 [[[[self ufw] appController] window] makeKeyAndVisible];
             }
         }
-        NSLog(@"SDK Initialized");
     }
     @catch (NSException *e) {
         NSLog(@"%@",e);
@@ -89,27 +99,39 @@ UnityFramework* UnityFrameworkLoad()
 }
 
 
-/// Call method to ARMOD SDK
-/// @param methodName Method name
-/// @param data method paramaters,It's a string
+/*!
+@Discussion Call method to ARMOD SDK
+@param methodName Method name
+@param data method paramaters,It's a string
+*/
 - (void)callSDKMethod:(char const*) methodName addData:(char const*)data{
     if([self armodIsInitialized]){
-        [[self ufw] sendMessageToGOWithName:"EntryPoint" functionName:methodName  message:data];
+        [[self ufw] sendMessageToGOWithName:messageReceiveObjecgtName functionName:methodName  message:data];
     }else{
         NSLog(@"You can not send anything message to AR,Because SDK is not initialize");
     }
 }
 
 
-/// unload the ARMOD Window
+/*!
+@Discussion Unload the ARMOD Window
+*/
 - (void)unloadARMODWindow{
     if([self armodIsInitialized]){
-//        [[self ufw] unloadApplication];
-        [self callSDKMethod:"Dispose" addData:""];
+        [self callSDKMethod:dispose addData:""];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.125 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [UnityFrameworkLoad() unloadApplication];
+            [[self ufw] unregisterFrameworkListener:self];
+            [self setUfw:nil];
+            [originWindow makeKeyAndVisible];
+        });
     }
 }
 
-/// Display the ARMOD Window
+/*!
+@Discussion Display the ARMOD Window
+*/
 - (void)showARMODWindow{
     if([self armodIsInitialized]){
         
@@ -119,39 +141,116 @@ UnityFramework* UnityFrameworkLoad()
     }
 }
 
-- (void)cleanCache{
-    if([self armodIsInitialized]){
-        [self callSDKMethod:"CleanCache" addData:""];
-    }
-}
-
-
-/// Execute after ARMOD and Unity unload
-/// @param notification notification description
-- (void)unityDidUnload:(NSNotification *)notification{
-    [[self ufw] unregisterFrameworkListener:self];
-    [self setUfw:nil];
-    [originWindow makeKeyAndVisible];
-}
-
-- (void)unityDidQuit:(NSNotification*)notification{
-    [[self ufw] unregisterFrameworkListener: self];
-    [self setUfw: nil];
-    [originWindow makeKeyAndVisible];
-}
-
+/*!
+@Discussion Register callback for app
+*/
 - (void) registerAPIforNativeCalls:(id<NativeCallsProtocol>) api{
     ncp = api;
 }
 
-// MARK: - API
+// MARK: - ARMOD Public Native APIs
 + (id)sharedInstance{
     static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[self alloc] init];
-    });
+    dispatch_once(&onceToken, ^{sharedInstance = [[self alloc] init];});
     return sharedInstance;
 }
+
+- (void) connectArgcArgv:(int)gArgc setgArgv:(char **) gArgv{
+    [self setSelfgArgc:gArgc];
+    [self setSelfgArgv:gArgv];
+}
+
+- (void) connectARMOD:(UIWindow*) nativeWindow{
+    originWindow = nativeWindow;
+}
+
+
+- (void) connectLaunchOpts:(NSDictionary*) applaunchOpts{
+    appLaunchOpts = applaunchOpts;
+}
+
+- (void)initARMOD:(NSString*) appconfigure{
+    [self initARMODModule];
+    [self callSDKMethod:initApp addData:[appconfigure UTF8String]];
+}
+
+/*!
+@Discussion Query the AR Experience project by project uid
+*/
+- (void)fetchProject:(NSString*) projecetUid{
+    //Since the initialization decision is performed asynchronously, the query item needs to be delayed
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.125 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        const char *converted_projectUid = [projecetUid UTF8String];
+        [self callSDKMethod:fetchProject addData:converted_projectUid];
+    });
+}
+
+/*!
+@Discussion Query the AR Experience project by image
+*/
+- (void)fetchProjectByImage{
+    //Since the initialization decision is performed asynchronously, the query item needs to be delayed
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.125 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self callSDKMethod:fetchProjectByImage addData:""];
+    });
+}
+
+/*!
+@Discussion Get current view controller
+*/
+- (UIViewController*)getARMODController{
+    return [[[self ufw] appController] rootViewController];
+}
+
+/*!
+@Discussion Load and show ARMOD view
+*/
+- (void)loadAndShowARMODView{
+    if(![self armodIsInitialized]){
+        [self initARMODModule];
+    }
+    
+    [self showARMODWindow];
+}
+
+/*!
+@Discussion Set current App orientation
+*/
+- (void)setUIInterfaceOrientation:(UIInterfaceOrientation)orientation{
+    NSString *orientationValue = [NSString stringWithFormat:@"%d",(int)orientation];
+    const char *data = [orientationValue UTF8String];
+    [self callSDKMethod:setUIInterfaceOrientation addData:data];
+}
+
+/*!
+@Discussion Unload and hide ARMOD view
+*/
+- (void)unloadAndHideARMODView{
+    if([self armodIsInitialized]){
+        [self unloadARMODWindow];
+    }else{
+        NSLog(@"ARMOD is not initialized ,Initialize ARMOD first");
+    }
+}
+
+/*!
+@Discussion Clean the ARMOD's AR experience assets
+*/
+- (void)cleanCache{
+    if([self armodIsInitialized]){
+        [self callSDKMethod:cleanCache addData:""];
+    }
+}
+
+/*!
+@Discussion Continue to download
+*/
+- (void)continueToDownloadARExperience{
+    if([self armodIsInitialized]){
+        [self callSDKMethod:continueToDownloadARExperience addData:""];
+    }
+}
+// MARK: - Application Hook
 
 -(void)applicationWillResignActive:(UIApplication *)application{
     if( [self armodIsInitialized]){
@@ -181,74 +280,6 @@ UnityFramework* UnityFrameworkLoad()
 - (void)applicationWillTerminate:(UIApplication *)application{
     if( [self armodIsInitialized]){
         [[[self ufw] appController] applicationWillTerminate:application];
-    }
-}
-
-
-- (void) connectArgcArgv:(int)gArgc setgArgv:(char **) gArgv{
-    [self setSelfgArgc:gArgc];
-    [self setSelfgArgv:gArgv];
-}
-
-- (void) connectARMOD:(UIWindow*) nativeWindow{
-    originWindow = nativeWindow;
-}
-
-
-- (void) connectLaunchOpts:(NSDictionary*) applaunchOpts{
-    appLaunchOpts = applaunchOpts;
-}
-
-
-- (void)initARMOD:(NSString*) appconfigure{
-    [self initARMODModule];
-    [self callSDKMethod:initApp addData:[appconfigure UTF8String]];
-}
-
-- (void)fetchProject:(NSString*) projecetUid{
-    //Since the initialization decision is performed asynchronously, the query item needs to be delayed
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.125 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        const char *converted_projectUid = [projecetUid UTF8String];
-        [self callSDKMethod:fetchProject addData:converted_projectUid];
-    });
-}
-
-
-- (void)fetchProjectByImage{
-    //Since the initialization decision is performed asynchronously, the query item needs to be delayed
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.125 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self callSDKMethod:fetchProjectByImage addData:""];
-        
-    });
-}
-
-
-- (UIViewController*)getARMODController{
-    return [[[self ufw] appController] rootViewController];
-}
-
-
-- (void)loadAndShowARMODView{
-    if(![self armodIsInitialized]){
-        [self initARMODModule];
-    }
-    
-    [self showARMODWindow];
-}
-
-
-- (void)setUIInterfaceOrientation:(UIInterfaceOrientation)orientation{
-    NSString *orientationValue = [NSString stringWithFormat:@"%d",(int)orientation];
-    const char *data = [orientationValue UTF8String];
-    [self callSDKMethod:"SetUIInterfaceOrientation" addData:data];
-}
-
-- (void)unloadAndHideARMODView{
-    NSLog(@"Unload and hide armod view executed!");
-    if([self armodIsInitialized]){
-        [self unloadARMODWindow];
-    }else{
-        NSLog(@"ARMOD is not initialized ,Initialize ARMOD first");
     }
 }
 @end
